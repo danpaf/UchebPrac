d6537be3898f6810de2af5c08dfb91c130033cb3
danpaf <65860266+danpaf@users.noreply.github.com>
2023-06-23 15:24:48 +0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -22,17 +22,15 @@ def parse_logs(log_file):
             content_length = int(match.group(5))
             user_agent = match.group(7)
 
-            # Преобразуем строку даты в объект datetime
+           
             date = datetime.strptime(date_str, "%d/%b/%Y:%H:%M:%S %z")
 
-            # Создаем запись в базе данных
+            
             AccessLog.create(ip_address=ip_address, date=date, http_method=http_method,
                             response_code=response_code, user_agent=user_agent)
 
     print("Логи успешно сохранены в базе данных.")
 
 
-# Точка входа в программу
-if __name__ == '__main__':
-    log_file = 'access_logs.txt'  # Укажите путь к файлу с логами
-    parse_logs(log_file)
+
+
